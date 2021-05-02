d700b4286ead9c10dd8ac45875f8092c4ce5dd40
shanse <shansea.s@gmail.com>
2021-05-02 23:04:52 +0300

    HW10+

--- src/lectionHW/Functional/ChangeList2.java
+++ src/lectionHW/Functional/ChangeList2.java
@@ -4,21 +4,25 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class ChangeList2 {
     public static void main(String[] args) throws IOException {
         List<String> readList = Files.readAllLines(Paths.get("src/lectionHW/Functional/Resources/File1.txt"));
-        Consumer<String> upCase = s -> s.toLowerCase();
+
+        /*Function<String, String> upCase = String::toUpperCase;
         for (String str : readList) {
-            upCase.accept(str);
+            upCase.apply(str);
         }
-        Collections.sort(readList);
+        Collections.sort(readList, Comparator.reverseOrder());*/
 
         //variant with stream
-        //readList = readList.stream().map(String::toUpperCase).sorted().collect(Collectors.toList());
+        readList = readList.stream().map(String::toUpperCase).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
 
         System.out.println(readList);
     }
